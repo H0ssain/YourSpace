8c26efd2bbd8dec8fc0112b841abdff96dce19de
Abrar Hossain <61503700+H0ssain@users.noreply.github.com>
2021-09-04 01:24:11 +0600

    Delete controller directory

--- controller/homeController.js
+++
@@ -1,9 +0,0 @@
-// get home page
-
-function getHome(req, res, next) {
-  res.render("index");
-}
-
-module.exports = {
-  getHome,
-};
--- controller/loginController.js
+++
@@ -1,9 +0,0 @@
-// get login page
-
-function getLogin(req, res, next) {
-  res.render("login");
-}
-
-module.exports = {
-  getLogin,
-};
